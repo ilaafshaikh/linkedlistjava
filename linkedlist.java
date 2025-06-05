686c1e56c41674b0f451021bd75063f53afa6262
ilaafshaikhgit config --global user.name ilaafshaikh <ilaafshaikh61@gmail.com>
2025-06-05 22:41:04 +0530

    first commit

---
+++ linkedlist.java
@@ -0,0 +1,18 @@
+public class LinkedList18{
+
+//creation of linkedlist
+    public static class Node{
+        //singly linked list
+    //     int data;
+    //     Node next;
+
+    //     public Node(int data){
+    //         this.data=data;
+    //         this.next=null;
+    //     }
+
+
+    // }
+    // public static Node head;
+    // public static Node tail;
+    //  public static int size;
